28f6add5fcdd6e643d88026ddc8830dbfd7a91cb
tom191 <tom191@ws307-075.schule.bremen.de>
2017-02-02 11:20:35 +0100

    pöüpütru

--- Informatik/src/eigeneProgramme/farben.java
+++ Informatik/src/eigeneProgramme/farben.java
@@ -7,9 +7,11 @@ import hilfe.*;
 
 public class farben extends HJFrame {
 	// globale Variablen
+	static final Color DunkelGrün = new Color(0,100,0);
+	static final Color Braun = new Color (139,69,19);
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 700;
-	static final Color beige= new Color(255,211,155);
+	static final Color beige = new Color(255,211,155);
 	private static final Color BACKGROUND = beige;
 	private static final Color FOREGROUND = Color.BLACK;
 
@@ -22,7 +24,18 @@ public class farben extends HJFrame {
 	@Override
 	public void myPaint(Graphics g) {
 		// wird aufgerufen, wenn das Fenster neu gezeichnet wird
-
+		g.setColor(DunkelGrün);
+		g.fillOval(20, 80, 50, 50);
+		g.setColor(Braun);
+		g.fillRect(40, 130, 10, 50);
+		g.setColor(DunkelGrün);
+		g.fillOval(100, 100, 50, 50);
+		g.setColor(Braun);
+		g.fillRect(120, 150, 10, 50);
+		g.setColor(DunkelGrün);
+		g.fillOval(200, 200, 50, 50);
+		g.setColor(Braun);
+		g.fillRect(220, 250, 10, 50);
 	}
 
 	public static void main(final String[] args) {
