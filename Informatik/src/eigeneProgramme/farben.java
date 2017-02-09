6fd242d900e374d9f1780100e7723f56d7c6c71c
tom191 <tom191@ws307-052.schule.bremen.de>
2017-02-09 11:27:09 +0100

    .

--- Informatik/src/eigeneProgramme/farben.java
+++ Informatik/src/eigeneProgramme/farben.java
@@ -7,35 +7,47 @@ import hilfe.*;
 
 public class farben extends HJFrame {
 	// globale Variablen
-	static final Color DunkelGrün = new Color(0,100,0);
-	static final Color Braun = new Color (139,69,19);
+	static final Color DunkelGrün = new Color(0, 100, 0);
+	static final Color Braun = new Color(139, 69, 19);
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 700;
-	static final Color beige = new Color(255,211,155);
+	static final Color beige = new Color(255, 211, 155);
 	private static final Color BACKGROUND = beige;
 	private static final Color FOREGROUND = Color.BLACK;
 
 	public farben(final String title) {
 		super(WIDTH, HEIGHT, BACKGROUND, FOREGROUND, title);
 		// eigene Initialisierung
-		
+
 	}
 
 	@Override
 	public void myPaint(Graphics g) {
 		// wird aufgerufen, wenn das Fenster neu gezeichnet wird
+		baum(g, 100, 100);
+		baum(g, 200, 50);
+		baum(g, 20, 80);
+		for (int x = 0; x < 5; x++){
+			baum(g, x * 60 + 400, 50);
+		}
+			int anzahlBäume = 0;
+			
+			for (int y = 0; y < 6; y++) {
+				for (int x = 0; x < 15; x++) {
+					anzahlBäume++;
+					if (anzahlBäume % 6 != 0) {
+						baum (g, x * 40 + 100, 220 + y * 70);
+					}
+				}
+			}
+		}
+
+	public void baum(Graphics g, int x, int y) {
 		g.setColor(DunkelGrün);
-		g.fillOval(20, 80, 50, 50);
+		g.fillOval(x, y, 50, 50);
 		g.setColor(Braun);
-		g.fillRect(40, 130, 10, 50);
-		g.setColor(DunkelGrün);
-		g.fillOval(100, 100, 50, 50);
-		g.setColor(Braun);
-		g.fillRect(120, 150, 10, 50);
-		g.setColor(DunkelGrün);
-		g.fillOval(200, 200, 50, 50);
-		g.setColor(Braun);
-		g.fillRect(220, 250, 10, 50);
+		g.fillRect(x+20, y+50, 10, 50);
+
 	}
 
 	public static void main(final String[] args) {
