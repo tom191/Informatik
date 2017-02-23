eb7db12ee291cb76de787e4a156d9dc95553c42c
tom191 <tom191@ws307-056.schule.bremen.de>
2017-02-23 11:31:19 +0100

    5z4z4eue5uii

--- Informatik/src/klassenundobjekte/HundAnwendung.java
+++ Informatik/src/klassenundobjekte/HundAnwendung.java
@@ -1,6 +1,7 @@
 package klassenundobjekte;
 
 import java.awt.Color;
+import javax.swing.Timer;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import hilfe.*;
@@ -12,17 +13,29 @@ public class HundAnwendung extends HJFrame {
 	private static final Color BACKGROUND = Color.WHITE;
 	private static final Color FOREGROUND = Color.BLACK;
 	public Hund max;
+	public Hund kevin;
+	public Hund lul;
+	public Hund aleks;
 	
 	public HundAnwendung(final String title) {
 		super(WIDTH, HEIGHT, BACKGROUND, FOREGROUND, title);
 		// eigene Initialisierung
+		Timer timer = new Timer(50, this);
+		timer.start();
 		max = new Hund(100, 30, Color.GREEN);
+		kevin = new Hund (200,60, Color.BLACK);
+		lul = new Hund (50, 60, Color.PINK);
+		aleks = new Hund (40,100, Color.RED);
 	}
 
 	@Override
 	public void myPaint(Graphics g) {
 		// wird aufgerufen, wenn das Fenster neu gezeichnet wird
 		max.springen(g);
+		kevin.springen(g);
+		lul.stehen(g);
+		aleks.stehen(g);
+		
 	}
 
 	public static void main(final String[] args) {
